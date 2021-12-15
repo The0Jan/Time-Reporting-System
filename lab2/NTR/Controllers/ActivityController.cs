b28b7836c95710f297d747b03448c7f5c506b659
Znachor <j.walczak@interia.pl>
2021-12-15 12:45:42 +0100

    added List activity month

--- lab2/NTR/Controllers/ActivityController.cs
+++ lab2/NTR/Controllers/ActivityController.cs
@@ -24,16 +24,22 @@ namespace NTR.Controllers
 
         public IActionResult List(DateTime? date)
         {
+            DateTime new_date;
             int id = Convert.ToInt32(Request.Cookies["users_UserModelId"]);
-            if (date.HasValue) {
-                ViewBag.date = date;
-                return View( _context.Activities.Where(m => m.UserModelId == id && m.Date == date));
+            if (date != null) {
+                new_date = (DateTime) date;
             } 
             else {
-                DateTime today = DateTime.Today;
-                ViewBag.date = today;
-                return View( _context.Activities.Where(m => m.UserModelId == id && m.Date == today));
+                new_date = DateTime.Today;
             }
+            ViewBag.date = new_date;
+            ViewBag.submitted = 0;
+            var partaking = _context.ProjectPartakes.FirstOrDefault(m => m.UserModelId == id && m.Year == new_date.Year && m.Month == new_date.Month);
+            if ( partaking != null && partaking.Submitted == true){
+                ViewBag.submitted = 1;
+            }
+            return View( _context.Activities.Where(m => m.UserModelId == id && m.Date == new_date));
+
         }
 
         public IActionResult Create(DateTime? date, string? ProjectName, string? Subcode, int? Time, String? Description )
@@ -49,22 +55,43 @@ namespace NTR.Controllers
         [HttpPost]
         public IActionResult Create(DateTime date, string ProjectName, string Subcode, int Time, string Description)
         {
-            ActivityModel activityModel = new ActivityModel();
-            var project = _context.Projects.FirstOrDefault(m => m.Title == ProjectName.ToUpper());
-            var subcode = _context.Subcodes.Where(m => m.ProjectModelId == project.ProjectModelId).FirstOrDefault(m => m.name == Subcode.ToLower());
-   
 
-            if(project==null || subcode==null){
+            if(ProjectName==null || Subcode == null)
+            {
                 return RedirectToAction("Create", "Activity", new {@date = date, @ProjectName = ProjectName, @Subcode=Subcode, @Time=Time, @Description=Description});
             }
-            else{
+            else
+            {
+                var project = _context.Projects.FirstOrDefault(m => m.Title == ProjectName.ToUpper());   
+                ActivityModel activityModel = new ActivityModel();
                 activityModel.UserModelId = Convert.ToInt32(Request.Cookies["users_UserModelId"]);
                 activityModel.ProjectModelId = project.ProjectModelId;
-                activityModel.SubcodeModelId = subcode.SubcodeModelId;
                 activityModel.Date = date;
                 activityModel.Time = Time;
                 activityModel.Description = Description;
                 activityModel.Frozen = false;
+
+
+                var subcode = _context.Subcodes.Where(m => m.ProjectModelId == project.ProjectModelId).FirstOrDefault(m => m.name == Subcode.ToLower());
+                activityModel.SubcodeModelId = subcode.SubcodeModelId;
+                
+                var partaking = _context.ProjectPartakes.Where(m => m.ProjectModelId == project.ProjectModelId && m.UserModelId == activityModel.UserModelId).FirstOrDefault();
+                if(partaking == null)
+                {
+                    ProjectPartake partakings = new ProjectPartake();
+                    partakings.ProjectModelId = project.ProjectModelId;
+                    partakings.UserModelId = activityModel.UserModelId;
+                    partakings.SubmittedTime = Time;
+                    partakings.Year = date.Year;
+                    partakings.Month = date.Month;
+                    partakings.Submitted = false;
+                    _context.Add(partakings);
+                }
+                else
+                {
+                    partaking.SubmittedTime += Time;
+                    _context.Update(partaking);
+                }
 
                 _context.Add(activityModel);
                 _context.SaveChanges();
@@ -87,6 +114,10 @@ namespace NTR.Controllers
 
         public IActionResult Delete(int id){
             var activity = _context.Activities.FirstOrDefault(m => m.ActivityModelId == id);
+            var partaking = _context.ProjectPartakes.FirstOrDefault(m => m.ProjectModelId == activity.ProjectModelId && m.UserModelId == Convert.ToInt32(Request.Cookies["users_UserModelId"]));
+            partaking.SubmittedTime -= activity.Time;
+
+            _context.ProjectPartakes.Update(partaking);
             _context.Activities.Remove(activity);
             _context.SaveChanges();
 
@@ -99,6 +130,7 @@ namespace NTR.Controllers
             var project = _context.Projects.FirstOrDefault(m => m.ProjectModelId == activity.ProjectModelId);
             var subcode = _context.Subcodes.FirstOrDefault(m => m.SubcodeModelId == activity.SubcodeModelId);
 
+
             ViewBag.project = project.Title;
             ViewBag.subcode = subcode.name;
             return View( activity);
@@ -107,12 +139,30 @@ namespace NTR.Controllers
         [HttpPost]
         public IActionResult Edit(int id, int Time, string Description){
             var activity = _context.Activities.FirstOrDefault(m => m.ActivityModelId == id);
+            var partaking = _context.ProjectPartakes.FirstOrDefault(m => m.ProjectModelId == activity.ProjectModelId && m.UserModelId == activity.UserModelId);
+            int Time_dif = Time - activity.Time;
+
             activity.Time = Time;
             activity.Description = Description;
 
+            partaking.SubmittedTime += Time_dif;
+
+            _context.Update(partaking);
             _context.Update(activity);
             _context.SaveChanges();
             return RedirectToAction("List", "Activity", new {@date = activity.Date});
+        }
+
+        public IActionResult Submit(DateTime date){
+            int id = Convert.ToInt32(Request.Cookies["users_UserModelId"]);
+            IEnumerable<ProjectPartake> partaking = _context.ProjectPartakes.Where(m => m.UserModelId == id && m.Year == date.Year && m.Month == date.Month);
+            foreach(var item in partaking){
+                item.Submitted = true;
+                _context.Update(item);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("List", "Activity", new {@date = date});
         }
 
     }
--- lab2/NTR/Controllers/ProjectController.cs
+++ lab2/NTR/Controllers/ProjectController.cs
@@ -21,10 +21,24 @@ namespace NTR.Controllers
             _context = context;
         }
 
+        public IActionResult List(){
+            return View();
+        }
+
         [HttpGet]
         public IActionResult List(int Year, int Month){
+            int id = Convert.ToInt32(Request.Cookies["users_UserModelId"]);
+            IEnumerable<ProjectPartake> partakings = _context.ProjectPartakes.Where(m => m.UserModelId == id && m.Year == Year && m.Month == Month).ToList();
+            List<string> project_names  = new List<string>();
+            
+            foreach(var partake in partakings){
+                string name = _context.Projects.FirstOrDefault(m=> m.ProjectModelId == partake.ProjectModelId).Title;
+                project_names.Add(name);
+            }
+            ViewBag.projects = project_names;
 
-            return View();
+
+            return View(partakings);
         }
 
 
